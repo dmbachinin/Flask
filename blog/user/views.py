bd3fa2cf613004b2db94c129e2ed88ef14a4855f
dm <d79123244656@yandex.ru>
2023-02-23 12:12:50 +0300

    Доработка шаблона для вывода пользователей с добавление навигации

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, render_template
+from werkzeug.exceptions import NotFound
 
 user = Blueprint('user', __name__, url_prefix="/users", static_folder="../static")
 
-USERS = ['Dima', 'Anna', 'Sasha', 'Anton']
+USERS = {
+    1: 'Dima',
+    2: 'Anna',
+    3: 'Sasha',
+    4: 'Anton'
+}
 
 
 @user.route('/')
@@ -10,6 +16,12 @@ def user_list():
     return render_template("users/list.html", users=USERS)
 
 
-@user.route('/<pk>')
-def get_users(pk: int):
-    return f"{pk}"
+@user.route('/<int:pk>')
+def get_user(pk: int):
+    try:
+        user_correct = USERS[pk]
+    except KeyError:
+        raise NotFound(f"User with id = {pk} not found")
+    return render_template("users/details.html", user=user_correct)
+
+
