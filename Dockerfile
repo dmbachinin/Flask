8af4dc8c57a2940180d534bc264c507b92f3fbb5
dm <d79123244656@yandex.ru>
2023-03-01 20:44:08 +0300

    Создание Doсkerfile и контейнера

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.8.10-buster
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+
+RUN pip3 install -r requirements.txt
+
+COPY wsgi.py wsgi.py
+COPY blog ./blog
+
+EXPOSE 5000
+
+CMD ["flask", "run"]
--- blog/app.py
+++ blog/app.py
@@ -3,16 +3,23 @@ from flask_login import LoginManager
 import json
 import os
 
+from flask_migrate import Migrate
+
 from blog.models.database import db
 
 
 login_manager = LoginManager()
+migrate = Migrate()
+
+
 def create_app() -> Flask:
     app = Flask(__name__)
 
     cfg_name = os.environ.get("CONFIG_NAME") or "BaseConfig"
     app.config.from_object(f"blog.configs.{cfg_name}")
+
     db.init_app(app)
+    migrate.init_app(app, db)
 
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
