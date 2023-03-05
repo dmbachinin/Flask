77f2af1f8bc0b32d1d607b320da953b060bc99b2
dm <d79123244656@yandex.ru>
2023-03-05 11:25:35 +0300

    Изменение модели пользователей

--- blog/models/user.py
+++ blog/models/user.py
@@ -8,7 +8,8 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    user_name = Column(String(80), nullable=False)
+    first_name = Column(String(255), nullable=False)
+    last_name = Column(String(255), nullable=False)
     email = Column(String(255), unique=True)
     is_staff = Column(Boolean, nullable=False, default=False)
     password = Column(String(255), nullable=False)
